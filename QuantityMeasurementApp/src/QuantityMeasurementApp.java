aec651c42ec2dc95a6d503666d81962fef2a96c3
CodeDevAyush <ayushjoshi692@gmail.com>
2026-04-27 10:10:00 +0530

    UC7: Addition with explicit target unit

--- QuantityMeasurementApp/src/QuantityMeasurementApp.java
+++ QuantityMeasurementApp/src/QuantityMeasurementApp.java
@@ -69,7 +69,8 @@ public class QuantityMeasurementApp {
         QuantityLength q1 = new QuantityLength(1.0, LengthUnit.FEET);
         QuantityLength q2 = new QuantityLength(12.0, LengthUnit.INCHES);
 
-        System.out.println(q1.add(q2));
+        System.out.println(QuantityLength.add(q1, q2, LengthUnit.FEET));
         System.out.println(QuantityLength.add(q1, q2, LengthUnit.INCHES));
+        System.out.println(QuantityLength.add(q1, q2, LengthUnit.YARDS));
     }
 }

56f0ba0c3dbfcf1c6025cffa17efff1a8b701a89
CodeDevAyush <ayushjoshi692@gmail.com>
2026-04-27 09:54:02 +0530

    UC4: Added Yards and Centimeters support using enum extension

--- QuantityMeasurementApp/src/QuantityMeasurementApp.java
+++ QuantityMeasurementApp/src/QuantityMeasurementApp.java
@@ -2,7 +2,9 @@ public class QuantityMeasurementApp {
 
     enum LengthUnit {
         FEET(1.0),
-        INCH(1.0 / 12.0);
+        INCH(1.0 / 12.0),
+        YARD(3.0),
+        CM(0.393701 / 12.0);
 
         private final double toFeet;
 
@@ -39,14 +41,9 @@ public class QuantityMeasurementApp {
     }
 
     public static void main(String[] args) {
-        Quantity q1 = new Quantity(1.0, LengthUnit.FEET);
-        Quantity q2 = new Quantity(12.0, LengthUnit.INCH);
-
-        System.out.println(q1.equals(q2));
-
-        Quantity q3 = new Quantity(1.0, LengthUnit.INCH);
-        Quantity q4 = new Quantity(1.0, LengthUnit.INCH);
-
-        System.out.println(q3.equals(q4));
+        System.out.println(new Quantity(1.0, LengthUnit.YARD).equals(new Quantity(3.0, LengthUnit.FEET)));
+        System.out.println(new Quantity(1.0, LengthUnit.YARD).equals(new Quantity(36.0, LengthUnit.INCH)));
+        System.out.println(new Quantity(2.0, LengthUnit.CM).equals(new Quantity(2.0, LengthUnit.CM)));
+        System.out.println(new Quantity(1.0, LengthUnit.CM).equals(new Quantity(0.393701, LengthUnit.INCH)));
     }
 }
